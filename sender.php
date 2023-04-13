f712f432e9221b9028b2110c1e3ca9dfbd204636
александр <85447129+Alexander-Guzhvin@users.noreply.github.com>
2023-04-13 17:29:36 +0300

    a

--- sender.php
+++ sender.php
@@ -1,21 +1,36 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $text = $_POST['text'];
 
-	$to = "alexxguzhvin@gmail.com"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = $email;
-	$subject = "Заявка c сайта";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-	
-	$msg="
-    Имя: $name /n
-    Почта: $email /n
-    Текст: $text"; 	
-	mail($to, $subject, $msg, "From: $from ");
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "alexxguzhvin@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+';
+
 
 ?>
-
-<p class='text-send' style="display: flex;justify-content: center;align-items: center;height: 80vh;font-size: 36px;">The form has been sent, I will contact you shortly</p>
